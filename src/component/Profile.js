c066f035cf9b808d5f80d759ff168eb4bcdd0bda
HFO4 <912394456@qq.com>
2019-03-14 10:00:42 +0800

    Feat: User profile

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { withStyles } from '@material-ui/core/styles';
 import { connect } from 'react-redux'
-import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import Avatar from '@material-ui/core/Avatar';
 import { toggleSnackbar,}from "../actions/index"
@@ -9,6 +8,15 @@ import Typography from '@material-ui/core/Typography';
 import axios from 'axios'
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import IconButton from '@material-ui/core/IconButton';
+import LeftIcon from '@material-ui/icons/KeyboardArrowLeft'
+import RighttIcon from '@material-ui/icons/KeyboardArrowRight'
+import Grid from '@material-ui/core/Grid';
 
 const styles = theme => ({
     layout: {
@@ -16,6 +24,7 @@ const styles = theme => ({
         marginTop:'50px',
         marginLeft: theme.spacing.unit * 3,
         marginRight: theme.spacing.unit * 3,
+        marginBottom: "30px",
         [theme.breakpoints.up("sm")]: {
           width: 700,
           marginLeft: 'auto',
@@ -46,8 +55,33 @@ const styles = theme => ({
         marginTop: "1px",
         fontSize: "25px",
         color: "#ffffffcf",
+    },
+    th:{
+        minWidth: "106px",
+    },
+    mobileHide:{
+        [theme.breakpoints.down("md")]: {
+            display:"none",
+        }
+    },
+    tableLink:{
+        cursor: "pointer",
+    },
+    navigator:{
+        display:"flex",
+        justifyContent:"space-between",
+    },
+    pageInfo:{
+        marginTop: "14px",
+        marginLeft: "23px",
+    },
+    infoItem:{
+        paddingLeft: "46px!important",
+        paddingBottom: "20px!important",
+    },
+    infoContainer:{
+        marginTop:"30px",
     }
-
 })
 const mapStateToProps = state => {
     return {
@@ -67,11 +101,46 @@ class ProfileCompoment extends Component {
 
     state={
         listType:0,
+        shareList:[],
+        page:1,
     }
 
     handleChange = (event, listType) => {
         this.setState({ listType });
-    };    
+        if(listType===1){
+            this.loadList(1,"hot");
+        }else if(listType===0){
+            this.loadList(1,"default");
+        }
+    };
+
+    componentDidMount = ()=>{
+        this.loadList(1,"default");
+    }
+
+    loadList = (page,shareType) => {
+        axios.post('/Profile/getList', {
+            uid:window.taegetUserInfo.uid,
+            type:shareType,
+            page:page,
+        }).then( (response)=> {
+            this.setState({
+                page:page,
+                shareList:response.data,
+            })
+        })
+        .catch((error) =>{
+            this.props.toggleSnackbar("top","right","加载失败","error");
+        });
+    }
+
+    loadNext = ()=>{
+        this.loadList(this.state.page+1,this.state.listType===0?"default":"hot");
+    }
+
+    loadPrev = ()=>{
+        this.loadList(this.state.page-1,this.state.listType===0?"default":"hot");
+    }
 
     render() {
         const { classes } = this.props;
@@ -99,6 +168,76 @@ class ProfileCompoment extends Component {
                     <Tab label="热门分享"/>
                     <Tab label="个人资料" />
                     </Tabs>
+                    {this.state.listType===2&&
+                    <div className={classes.infoContainer}>
+                        <Grid container spacing={24}>
+                            <Grid item md={4} xs={12} className={classes.infoItem}>
+                                <Typography color="textSecondary" variant="h6">UID</Typography>
+                                <Typography>{window.taegetUserInfo.uid}</Typography>
+                            </Grid>
+                            <Grid item md={4} xs={12} className={classes.infoItem}>
+                                <Typography color="textSecondary" variant="h6">昵称</Typography>
+                                <Typography>{window.taegetUserInfo.nickname}</Typography>
+                            </Grid>
+                            <Grid item md={4} xs={12} className={classes.infoItem}>
+                                <Typography color="textSecondary" variant="h6">用户组</Typography>
+                                <Typography>{window.taegetUserInfo.group}</Typography>
+                            </Grid>
+                            <Grid item md={4} xs={12} className={classes.infoItem}>
+                                <Typography color="textSecondary" variant="h6">分享总数</Typography>
+                                <Typography>{window.taegetUserInfo.shareCount}</Typography>
+                            </Grid>
+                            <Grid item md={4} xs={12} className={classes.infoItem}>
+                                <Typography color="textSecondary" variant="h6">注册日期</Typography>
+                                <Typography>{window.taegetUserInfo.regDate}</Typography>
+                            </Grid>
+                        </Grid>
+                    </div>}
+                    {(this.state.listType===0||this.state.listType===1)&&<div>
+                        <Table className={classes.table}>
+                            <TableHead>
+                            <TableRow>
+                                <TableCell>文件名</TableCell>
+                                <TableCell className={classes.mobileHide}>分享日期</TableCell>
+                                <TableCell className={[classes.th,classes.mobileHide]}>下载次数</TableCell>
+                                <TableCell className={[classes.th,classes.mobileHide]}>浏览次数</TableCell>
+                            </TableRow>
+                            </TableHead>
+                            <TableBody>
+                            {this.state.shareList.map(row => (
+                                <TableRow 
+                                key={row.id} 
+                                className={classes.tableLink}
+                                onClick = {()=>window.open("/s/"+row.share_key)}
+                                >
+                                <TableCell><Typography>{row.fileData}</Typography></TableCell>
+                                <TableCell className={classes.mobileHide}>{row.share_time}</TableCell>
+                                <TableCell className={classes.mobileHide}>{row.download_num}</TableCell>
+                                <TableCell className={classes.mobileHide}>{row.view_num}</TableCell>
+                                </TableRow>
+                            ))}
+                            </TableBody>
+                        </Table>
+                        {(this.state.shareList.length!==0 && this.state.listType===0)&&
+                            <div className={classes.navigator}>
+                                <Typography color="textSecondary" className={classes.pageInfo}>第{this.state.page}页</Typography>
+                                <div>
+                                    <IconButton 
+                                    disabled={this.state.page===1} 
+                                    onClick={this.loadPrev}
+                                    >
+                                        <LeftIcon/>
+                                    </IconButton>
+                                    <IconButton 
+                                    disabled={this.state.shareList.length<10}
+                                    onClick={this.loadNext}
+                                    >
+                                        <RighttIcon/>
+                                    </IconButton>
+                                </div>
+                            </div>
+                        }
+                    </div>}
                 </Paper>
             </div>
         );
